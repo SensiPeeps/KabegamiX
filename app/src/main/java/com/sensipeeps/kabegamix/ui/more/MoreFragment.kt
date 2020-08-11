323f9d2f88cdec81d5fddedaa1fe53a0792eafda
fakeyatogod <fakeyatokami@gmail.com>
2020-08-11 13:17:36 +0530

    Refractor into Home, Fav & More fragments

--- app/src/main/java/com/sensipeeps/kabegamix/ui/more/MoreFragment.kt
+++ app/src/main/java/com/sensipeeps/kabegamix/ui/more/MoreFragment.kt
@@ -1,4 +1,4 @@
-package com.sensipeeps.kabegamix.ui.notifications
+package com.sensipeeps.kabegamix.ui.more
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,13 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.sensipeeps.kabegamix.R
 
-class NotificationsFragment : Fragment() {
+class MoreFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_notifications, container, false)
+        return inflater.inflate(R.layout.fragment_more, container, false)
     }
 }
