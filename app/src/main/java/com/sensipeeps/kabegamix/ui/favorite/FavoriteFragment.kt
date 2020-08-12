23009612e0be596f010c997ecd3490ddeda4997f
Shashank Baghel <theradcolor@gmail.com>
2020-08-12 10:21:50 +0530

    favfrag: log print a sample data from json
    Signed-off-by: Shashank Baghel <theradcolor@gmail.com>

--- app/src/main/java/com/sensipeeps/kabegamix/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/sensipeeps/kabegamix/ui/favorite/FavoriteFragment.kt
@@ -1,19 +1,28 @@
 package com.sensipeeps.kabegamix.ui.favorite
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.sensipeeps.kabegamix.R
+import com.sensipeeps.kabegamix.utils.jsonStructure
 
 class FavoriteFragment : Fragment() {
+
+    lateinit var jsonStructure: jsonStructure
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        val view =  inflater.inflate(R.layout.fragment_favorite, container, false)
+        jsonStructure = jsonStructure(context)
+        Log.d("FAV_FRAG", "author " + jsonStructure.retrieve(0, "wall_sam.json")[0] +
+                                    " url " + jsonStructure.retrieve(0, "wall_sam.json")[1] +
+                                    " name " + jsonStructure.retrieve(0, "wall_sam.json")[2] )
+        return view;
     }
 }
